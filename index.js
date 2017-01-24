91203f01d5110258f34b1235d49a16c6ec1de827
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-24 17:59:10 -0600

    added route functionality.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var koa = require('koa');
-var app = koa();
+const _ = require('koa-route');
+const koa = require('koa');
+const app = koa();
 
-app.use(function *(){
-  console.log('**********');
-  console.log('**********');
-});
+const db = {
+  tobi: { name: 'tobi', species: 'ferret' },
+  loki: { name: 'loki', species: 'ferret' },
+  jane: { name: 'jane', species: 'ferret' }
+};
+
+const petsHandler = {
+  list: function *(){
+    const names = Object.keys(db);
+    console.log(`pets: ${names.join(', ')}`);
+    this.response = 200;
+  },
+};
+
+app.use(_.get('/pets', petsHandler.list));
+app.use(_.get('/pets/:name', petsHandler.show));
 
 app.listen(3000);
+console.log('listening on port 3000');
